faa70257e812dbba65fd8403dfdcc76d26d6ea13
Chimoha31 <mokochii1108@gmail.com>
2021-10-28 15:03:15 -0700

    create components for react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from './components/Home';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => {
+  return(
+    <BrowserRouter>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/Menu">Menu</Link>
+      </li>
+      <li>
+        <Link to="/Contact">Contact</Link>
+      </li>
+    </ul>
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/Menu"><Menu /></Route>
+        <Route path="/Contact"><Contact /></Route>
+      </Switch>
+    </BrowserRouter>
+    )   
+};
 export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () => {
+  return(
+    <>
+    <p>Menu</p>
+    </>
+  )
+}
+
+export default Menu;
